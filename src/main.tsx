929f395dbf3bb75c4abfc908c0363fb3c273d87a
pcktm <jakub@kopanko.com>
2023-09-09 19:18:57 +0200

    feat: import workbox virtual module

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,35 @@ import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// eslint-disable-next-line import/no-unresolved
+import {registerSW} from 'virtual:pwa-register';
 import App from './App.tsx';
 import ErrorBoundary from './utils/ErrorBoundary.tsx';
+
+const intervalMS = 60 * 60 * 1000;
+
+const updateSW = registerSW({
+  onRegisteredSW(swUrl, r) {
+    if (r) {
+      setInterval(async () => {
+        if (!(!r.installing && navigator)) { return; }
+
+        if (('connection' in navigator) && !navigator.onLine) { return; }
+
+        const resp = await fetch(swUrl, {
+          cache: 'no-store',
+          headers: {
+            cache: 'no-store',
+            'cache-control': 'no-cache',
+          },
+        });
+
+        if (resp?.status === 200) { await r.update(); }
+      }, intervalMS);
+    }
+  },
+  immediate: true,
+});
 
 const themeConfig = {
   initialColorMode: 'light',
